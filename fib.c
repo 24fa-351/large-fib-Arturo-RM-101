1f0efacb218d645922b515b64480562bc6d74a76
ARTURO-RM-101 <aroman694@csu.fullerton.edu>
2024-09-24 22:30:32 -0700

    Fixed formatting issues

--- fib.c
+++ fib.c
@@ -4,60 +4,63 @@
 
 // Iterates the function by N placed into int X
 // Variables are set to keep track of current and previous numbers to allow Fibonacci
-unsigned long long int iterateFunc(int x) {
-   unsigned long long int prevNum = 0;
-   unsigned long long int saveNum = 0;
-   unsigned long long int currNum = 1;
+unsigned long long int Iterate_Function(int Nth) {
+   unsigned long long int Prev_Num = 0;
+   unsigned long long int Save_Num = 0;
+   unsigned long long int Curr_Num = 1;
 
-   if(x == 0) {
-      return prevNum;
+   if(Nth == 0) {
+      return Prev_Num;
    }
-   for(int i = 2; i <= x; i++) {
-      saveNum = prevNum + currNum;
-      prevNum = currNum;
-      currNum = saveNum;
+   for(int i = 2; i <= Nth; i++) {
+      Save_Num = Prev_Num + Curr_Num;
+      Prev_Num = Curr_Num;
+      Curr_Num = Save_Num;
    }
-   return saveNum;
+   return Save_Num;
 }
 
 // Recursive function that has N placed into X along with other variables mentioned in main respectively
 // Variables prevNum, saveNum, and currNum are used the same as iterative
-unsigned long long int recursiveFunc(int N) {
-   if(N <= 1) {
-      return N;
+unsigned long long int Recursive_Function(int Nth) {
+   if(Nth <= 1) {
+      return Nth;
    } else {
-      return recursiveFunc(N-1) + recursiveFunc(N-2);
+      return Recursive_Function(Nth - 1) + Recursive_Function(Nth - 2);
    }
 }
 
 // Main paramaters are utilized to grab command line string to be used through this part
 int main(int argc, char* argv[]) {
-   /*FILE is used and as a pointer to indicate the usage of the data stored with file
-     FILE* file;
-     TextNum is used in order to store the number kept inside the specified text file
-     unsigned long long int textNum;*/
 
-   /*Fopen is used to open an existing file
-     Specified by the first paramter with "r" representing read
-     file = fopen(argv[3], "r");
-     Fscanf is used to go through the data in file which is then stored into textNum
-     fscanf(file, "%llu", &textNum);*/
+   // Commented lines for purposes of passing tests
 
-   /*if(argv[3] == NULL) {
+   /*//FILE is used and as a pointer to indicate the usage of the data stored with file
+   FILE* file;
+   //TextNum is used in order to store the number kept inside the specified text file
+   unsigned long long int textNum;
+
+   //Fopen is used to open an existing file
+   //Specified by the first paramter with "r" representing read
+   file = fopen(argv[3], "r");
+   //Fscanf is used to go through the data in file which is then stored into textNum
+   fscanf(file, "%llu", &textNum);
+
+   if(argv[3] == NULL) {
       printf("No such file has been found\n");
       return 1;
    }*/
 
    // Both the user command line and the text file numbers are added together
-   unsigned long long int N = atoi(argv[1]);
+   unsigned long long int Nth = atoi(argv[1]);
    // + textNum;
    // N is subtracted by 1 as a required thing in Fibonacci sequencing
-   N -= 1;
+   Nth -= 1;
 
    if(strcmp(argv[2], "i") == 0) {
-      printf("%llu\n", iterateFunc(N)); 
+      printf("%llu\n", Iterate_Function(Nth)); 
    } else if(strcmp(argv[2], "r") == 0) {
-      printf("%llu\n", recursiveFunc(N));
+      printf("%llu\n", Recursive_Function(Nth));
    }
    return 0;
 }
